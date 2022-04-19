0be6fe08cfea292a4f37396e2ca79affa08a9898
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-04-19 14:55:28 -0500

    pre-review, Kanye West and Ron Swanson API conversation

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            for (int i = 0; i < 5; i++)
+            {
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+                Console.WriteLine(JObject.Parse(kanyeResponse).GetValue("quote").ToString());
+                Console.WriteLine();
+
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+
+                Console.WriteLine(JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim());
+                Console.WriteLine();
+            }
+        }
+    }
+}
